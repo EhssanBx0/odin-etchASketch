9927ac7672eb9cc718f10c9f3a78f3e856ef626d
EhssanBx0 <ehssanbaghban@outlook.com>
2025-08-28 23:55:50 +0100

    create func. and eventlistener for colour hover effect

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 let sketchContainer = document.createElement("div");
 sketchContainer.classList.add("container");
 
+function hoverColour(event){
+    console.dir(event.target);
+    event.target.classList.add("activeHover");
+}
+
 for (let i=0; i < 16*16; i++){
     let square = document.createElement("div");
     square.classList.add("square");
+    square.addEventListener("mouseover", hoverColour)
     sketchContainer.append(square);
 }
 
