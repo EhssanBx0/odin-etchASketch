17bcdecdcc80c5c060030cc398f7c446f8629073
EhssanBx0 <ehssanbaghban@outlook.com>
2025-08-29 09:20:36 +0100

    refactor grid creation into func. and add grid deletion func.

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
-let sketchContainer = document.createElement("div");
-sketchContainer.classList.add("container");
-
 function hoverColour(event){
-    console.dir(event.target);
     event.target.classList.add("activeHover");
 }
 
-for (let i=0; i < 16*16; i++){
-    let square = document.createElement("div");
-    square.classList.add("square");
-    square.addEventListener("mouseover", hoverColour)
-    sketchContainer.append(square);
+function deleteOldGrid(){
+    let body = document.querySelector("body");
+    let sketchContainer = document.querySelector(".container");
+    body.removeChild(sketchContainer);
 }
 
-let body = document.querySelector("body");
-body.append(sketchContainer);
+function createNewGrid(size=16){
+    let sketchContainer = document.createElement("div");
+    sketchContainer.classList.add("container");
+
+    for (let i=0; i < size**2; i++){
+        let square = document.createElement("div");
+        square.classList.add("square");
+        square.addEventListener("mouseover", hoverColour)
+        sketchContainer.append(square);
+    }
+
+    let body = document.querySelector("body");
+    body.append(sketchContainer);
+}
